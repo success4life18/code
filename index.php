cafc597ca2e9826ea441d9ddbbd89f2d5a6d1467
Success <success4life18@gmail.com>
2019-09-20 17:07:02 +0100

    added index.php

--- index.php
+++ index.php
@@ -1,42 +1,28 @@
-<?php if (isset($_POST['submit']) && $_SERVER['REQUEST_METHOD1'] == 'POST') {
+<?php
+// Initialize the session
+session_start();
+ 
 
-        $parser = file_get_contents("user.json");
-        $decode_details = json_decode($parser, true);
-        
-        function cleanUsersTextInput($data) { 
-            $data = trim($data);
-            $data = stripcslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-        }
-        // add your form here
-        $email = cleanUsersTextInput($_POST['email']);
-        $password = cleanUsersTextInput($_POST['password']);
-        
-        if (empty($_POST['email']) || empty($_POST['password'])) {
-            echo "Email or password is empty";
-            exit();
-        }
-        
-        else {
-            
-            $user = null;
-            $mail = null;
-            
-            
-            foreach ($decode_details['users'] as $result) {
-                
-                if ($result['email'] === $email && $result['password'] === $password) {
-                    $user = $result['username'];
-                    $mail = $result['email'];
-                }
-            }
 
-            if (!$user && !$mail) {
-                echo "Invalid login details, please try again!";
-                exit();
-            } else {
-
-                echo "Login Successful! You Made It To CodeStrong";
-            }
-        }
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hi,  Welcome to our site.</h1>
+    </div>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
